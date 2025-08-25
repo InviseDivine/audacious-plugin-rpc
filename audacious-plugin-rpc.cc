11517944210926629a0e2a2e90384fcee90708e3
InviseDivine <grisagorbacev65@gmail.com>
2025-08-25 23:35:34 +0200

    Its works! TODO: Albums cover...

--- audacious-plugin-rpc.cc
+++ audacious-plugin-rpc.cc
@@ -13,19 +13,17 @@
 #include <discord_rpc.h>
 
 #define EXPORT __attribute__((visibility("default")))
-#define APPLICATION_ID "484736379171897344"
-
-static const char *SETTING_EXTRA_TEXT = "extra_text";
+#define APPLICATION_ID "1408523003532279828"
 
 class RPCPlugin : public GeneralPlugin {
-
 public:
     static const char about[];
     static const PreferencesWidget widgets[];
     static const PluginPreferences prefs;
-
+    
+    // Info about plugin
     static constexpr PluginInfo info = {
-        N_("Discord RPC"),
+        N_("Discord RPC Plugin"),
         "audacious-plugin-rpc",
         about,
         &prefs
@@ -40,65 +38,72 @@ public:
 EXPORT RPCPlugin aud_plugin_instance;
 
 DiscordEventHandlers handlers;
-DiscordRichPresence presence;
-std::string fullTitle;
-std::string playingStatus;
 
 void init_discord() {
+    // Initializating Discord RPC
     memset(&handlers, 0, sizeof(handlers));
     Discord_Initialize(APPLICATION_ID, &handlers, 1, NULL);
 }
 
-void update_presence() {
-    Discord_UpdatePresence(&presence);
-}
-
-void init_presence() {
-    memset(&presence, 0, sizeof(presence));
-    presence.state = "Initialized";
-    presence.details = "Waiting...";
-    presence.largeImageKey = "logo";
-    presence.smallImageKey = "stop";
-    update_presence();
-}
-
 void cleanup_discord() {
+    // Clearing and shutting down Discord RPC
     Discord_ClearPresence();
     Discord_Shutdown();
 }
 
 void title_changed() {
+    // Do nothing when audacious doesn't ready
     if (!aud_drct_get_ready()) {
         return;
     }
+    // Creating presence
+    DiscordRichPresence presence;
+    memset(&presence, 0, sizeof(presence));
+    // Some vars
+    bool isHidePresence = aud_get_bool(nullptr, "isHidePresence");
 
-    if (aud_drct_get_playing()) {
-        bool paused = aud_drct_get_paused();
-        Tuple tuple = aud_drct_get_tuple();
-        String artist = tuple.get_str(Tuple::Artist);
-        std::string title(tuple.get_str(Tuple::Title));
+    Tuple tuple = aud_drct_get_tuple();
 
-        if (artist) {
-            fullTitle = (std::string(artist) + " - " + title).substr(0, 127);
-        } else {
-            fullTitle = title.substr(0, 127);
-        }
+    String title = tuple.get_str(Tuple::Title);
+    String artist = tuple.get_str(Tuple::Artist);
+    String album = tuple.get_str(Tuple::Album);
+    int64_t startTime = tuple.get_int(Tuple::StartTime);
 
-        playingStatus = paused ? "Paused" : "Listening";
+    // Setting information about artist and album
+    presence.state = artist;
+    presence.largeImageText = album;
 
-        presence.details = fullTitle.c_str();
-        presence.smallImageKey = paused ? "pause" : "play";
+    // Setting start timestamp
+    presence.startTimestamp = time(0);
+
+    // Setting activity to listening
+    presence.activityType = DISCORD_ACTIVITY_LISTENING;
+
+    if (!aud_drct_get_paused()) {
+        // Setting information about song title and song length
+        presence.details = title;
+
+        // Audacious provides song length in milliseconds, so we divide by 1000
+        presence.endTimestamp = time(0) + (aud_drct_get_length() / 1000);
+
+        // Updating activity status
+        Discord_UpdatePresence(&presence);
     } else {
-        playingStatus = "Stopped";
-        presence.state = "Stopped";
-        presence.smallImageKey = "stop";
+        // If isHidePresence true, just clearing Discord RPC
+        if (isHidePresence) {
+            Discord_ClearPresence();
+        } else {
+            // Clearing song length
+            presence.endTimestamp = 0;
+            
+            // Creating std::string to append "paused" prefix
+            std::string buf(title);
+            presence.details = buf.append(" (paused)").c_str();
+
+            // Updating activity status
+            Discord_UpdatePresence(&presence);
+        }
     }
-
-    std::string extraText(aud_get_str("audacious-plugin-rpc", SETTING_EXTRA_TEXT));
-    playingStatus = (playingStatus + " " + extraText).substr(0, 127);
-
-    presence.state = playingStatus.c_str();
-    update_presence();
 }
 
 void update_title_presence(void*, void*) {
@@ -106,43 +111,51 @@ void update_title_presence(void*, void*) {
 }
 
 void open_github() {
-   system("xdg-open https://github.com/darktohka/audacious-plugin-rpc");
+    // Linux rofls
+    system("xdg-open https://github.com/InviseDivine/audacious-plugin-rpc");
 }
 
 bool RPCPlugin::init() {
+    // Associating this events (hooks) with our function and initing Discord RPC
     init_discord();
-    init_presence();
+
     hook_associate("playback ready", update_title_presence, nullptr);
     hook_associate("playback end", update_title_presence, nullptr);
     hook_associate("playback stop", update_title_presence, nullptr);
     hook_associate("playback pause", update_title_presence, nullptr);
     hook_associate("playback unpause", update_title_presence, nullptr);
     hook_associate("title change", update_title_presence, nullptr);
+    hook_associate("tuple change", update_title_presence, nullptr);
+
     return true;
 }
 
 void RPCPlugin::cleanup() {
+    // Disassociating this events (hooks) with our function and cleaning discord
+    cleanup_discord();
+
     hook_dissociate("playback ready", update_title_presence);
     hook_dissociate("playback end", update_title_presence);
     hook_dissociate("playback stop", update_title_presence);
     hook_dissociate("playback pause", update_title_presence);
     hook_dissociate("playback unpause", update_title_presence);
     hook_dissociate("title change", update_title_presence);
-    cleanup_discord();
+    hook_dissociate("tuple change", update_title_presence);
 }
 
-const char RPCPlugin::about[] = N_("Discord RPC music status plugin\n\nWritten by: Derzsi Daniel <daniel@tohka.us>");
+// About developers
+const char RPCPlugin::about[] = N_(\
+"Discord RPC music status plugin\n\n\
+Written by: Derzsi Daniel <daniel@tohka.us>\n\n\
+Fork: by InviseDivine <invisedivine@sffempire.ru>");
 
 const PreferencesWidget RPCPlugin::widgets[] =
 {
-  WidgetEntry(
-      N_("Extra status text:"),
-      WidgetString("audacious-plugin-rpc", SETTING_EXTRA_TEXT, title_changed)
-  ),
-  WidgetButton(
-      N_("Fork on GitHub"),
-      {open_github}
-  )
+    WidgetCheck (N_("Hide presence when pause"),
+      WidgetBool(0, "isHidePresence", title_changed)),
+
+    WidgetButton(N_("Fork on GitHub"),
+        {open_github})
 };
 
 const PluginPreferences RPCPlugin::prefs = {{ widgets }};
