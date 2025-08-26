d89c623ad25dbdd24f4a7e5399169c781c19c2db
InviseDivine <grisagorbacev65@gmail.com>
2025-08-26 14:55:28 +0200

    albuum cooooover

--- audacious-plugin-rpc.cc
+++ audacious-plugin-rpc.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <format>
 
 #include <libaudcore/drct.h>
 #include <libaudcore/i18n.h>
@@ -11,6 +12,7 @@
 #include <libaudcore/runtime.h>
 
 #include <discord_rpc.h>
+#include <cpr/cpr.h>
 
 #define EXPORT __attribute__((visibility("default")))
 #define APPLICATION_ID "1408523003532279828"
@@ -39,6 +41,47 @@ EXPORT RPCPlugin aud_plugin_instance;
 
 DiscordEventHandlers handlers;
 
+std::string tempAlbum = "";
+std::string albumCover = "";
+
+std::string replaceSpaces(const std::string& originalString, const std::string& replacement) {
+    std::string newString = ""; // Initialize an empty string to store the result
+    for (char c : originalString) { // Iterate through each character of the original string
+        if (c == ' ') { // If the current character is a space
+            newString += replacement; // Append the replacement string
+        } else {
+            newString += c; // Append the character as is
+        }
+    }
+    return newString; // Return the modified string
+}
+
+void set_album_cover(std::string artist, std::string album) {
+    // Formatting strings to get working url
+    std::string formattedArtist = replaceSpaces(artist, "%20");
+    std::string formattedAlbum = replaceSpaces(album, "%20");
+
+    // Creating url
+    std::string url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo";
+    url.append("&api_key=5a499a2308896d61c7a8977e9bd3936f");
+    url.append(std::format("&artist={}&album={}", formattedArtist, formattedAlbum));
+
+    // Sending GET request
+    cpr::Response r = cpr::Get(cpr::Url{url});
+    
+    // Getting url to mega album cover
+    std::string megastr = "<image size=\"mega\">";
+    std::string megaendstr = "</image>";
+
+    auto megapos = r.text.find(megastr);
+    auto megaend = r.text.find(megaendstr, megapos);
+    
+    // Our album cover string
+    std::string albumCvr = r.text.substr(megapos + megastr.size(), megaend - megapos - megastr.size());
+
+    // Set global url to new url
+    albumCover = albumCvr;
+}
 void init_discord() {
     // Initializating Discord RPC
     memset(&handlers, 0, sizeof(handlers));
@@ -56,18 +99,20 @@ void title_changed() {
     if (!aud_drct_get_ready()) {
         return;
     }
+
     // Creating presence
     DiscordRichPresence presence;
     memset(&presence, 0, sizeof(presence));
+    
     // Some vars
     bool isHidePresence = aud_get_bool(nullptr, "isHidePresence");
 
+    // Current song data
     Tuple tuple = aud_drct_get_tuple();
 
     String title = tuple.get_str(Tuple::Title);
     String artist = tuple.get_str(Tuple::Artist);
     String album = tuple.get_str(Tuple::Album);
-    int64_t startTime = tuple.get_int(Tuple::StartTime);
 
     // Setting information about artist and album
     presence.state = artist;
@@ -78,6 +123,14 @@ void title_changed() {
 
     // Setting activity to listening
     presence.activityType = DISCORD_ACTIVITY_LISTENING;
+    
+    // Setting album cover
+    if (std::string(album) != tempAlbum) {
+        set_album_cover(std::string(artist), std::string(album));
+        tempAlbum = std::string(album);
+    }
+    presence.largeImageKey = albumCover.c_str();
+    presence.largeImageText = album;
 
     if (!aud_drct_get_paused()) {
         // Setting information about song title and song length
@@ -147,8 +200,9 @@ void RPCPlugin::cleanup() {
 const char RPCPlugin::about[] = N_(\
 "Discord RPC music status plugin\n\n\
 Written by: Derzsi Daniel <daniel@tohka.us>\n\n\
-Fork: by InviseDivine <invisedivine@sffempire.ru>");
+Fork by: InviseDivine <invisedivine@sffempire.ru>");
 
+// Settings
 const PreferencesWidget RPCPlugin::widgets[] =
 {
     WidgetCheck (N_("Hide presence when pause"),
